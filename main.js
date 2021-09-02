15426ee6b6b3e2f6065862a72d2559d83945fdff
Michael Tutor <tutor.michael78@gmail.com>
2021-09-02 11:52:11 -0500

    added test logic to query database

---
+++ main.js
@@ -0,0 +1,44 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySql username,
+        user: 'root',
+        // Your MySql password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+menu = async () => {
+    const response = await inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'search',
+                message: 'Would you like to search the database?',
+                choices: ['Yes', 'No']
+            }
+        ]);
+    
+    if (response.search == 'Yes') {
+        db.query('SELECT * FROM candidates', (err, rows) => {
+            if (err) {
+                console.log(err);
+            }
+
+            console.table(rows);
+            menu();
+            return;
+        });
+    } else {
+        db.end();
+        return;
+    }
+};
+
+menu();
