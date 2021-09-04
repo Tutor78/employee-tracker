3d45c75880c3721e9b8da288cef7bcb0669a2f75
Michael Tutor <tutor.michael78@gmail.com>
2021-09-04 17:46:53 -0500

    refactored code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const { allEmployees, employeeDepartments, employeeManagers, employeeRoles } = require('./lib/menuFunctions');
+
 
 require('dotenv').config();
 
@@ -43,146 +45,30 @@ menu = async () => {
         ]);
     
     if (response.search === 'View All Employees') {
-        const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department
-                        FROM employee
-                        JOIN role ON employee.role_id = role.id
-                        JOIN department ON role.department_id = department.id`;
-
-        db.query(sql, (err, rows) => {
-            if (err) {
-                console.log(err);
-            }
-
-            console.table(rows);
-            menu();
-            return;
-        });
+        allEmployees(db);
     } else if (response.search === 'View All Employees By Department') {
-        db.query(`SELECT department.name FROM department`, (err, result) => {
-            const departmentArr = [];
-
-            for (let i = 0; i < result.length; i++) {
-                departmentArr.push(result[i].name);
-            }
-
-            inquirer.prompt([
-                {
-                    type: 'list',
-                    name: 'department',
-                    message: 'Which department would you like to search by?',
-                    choices: departmentArr
-                }
-            ]).then(response => {
-                const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department
-                                FROM employee
-                                JOIN role on employee.role_id = role.id
-                                JOIN department ON role.department_id = department.id
-                                WHERE department.name = ?`;
-
-                let params = response.department;
-    
-                db.query(sql, params, (err, rows) => {
-                    if (err) {
-                        console.log(err);
-                    }
-    
-                    console.table(rows);
-                    menu();
-                    return;
-                })
-            })
-            
-        })
+        employeeDepartments(db);
     } else if (response.search === 'View All Employees By Manager') {
-        db.query(`SELECT employee.id, employee.first_name, employee.last_name FROM employee WHERE employee.manager_id IS NULL`, (err, response) => {
-            const managerArr = [];
-
-            for (let i = 0; i < response.length; i++) {
-                let managerName = response[i].first_name + ' ' + response[i].last_name;
-                let managerId = response[i].id
-
-                const managerObj = {};
-                managerObj.name = managerName;
-                managerObj.id = managerId;
-
-                managerArr.push(managerObj);
-            }
-
-            inquirer.prompt([
-                {
-                    type: 'list',
-                    name: 'manager',
-                    message: 'Which manager would you like to search by?',
-                    choices: managerArr
-                }
-            ]).then(response => {
-                const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department
-                                FROM employee
-                                JOIN role on employee.role_id = role.id
-                                JOIN department ON role.department_id = department.id
-                                WHERE employee.manager_id = ?`;
-                
-                let params;
-
-                for (let i = 0; i < managerArr.length; i++) {
-                    if (response.manager === managerArr[i].name) {
-                        params = managerArr[i].id;
-                    }
-                }
-    
-                db.query(sql, params, (err, rows) => {
-                    if (err) {
-                        console.log(err);
-                    }
-    
-                    console.table(rows);
-                    menu();
-                    return;
-                })
-            })
-        })
+        employeeManagers(db);
     } else if (response.search === 'View All Employees By Role') {
-        db.query(`SELECT role.title FROM role`, (err, response) => {
-            if (err) {
-                console.log(err);
-            }
+        employeeRoles(db);
+    } else if (response.search === 'Add Employee') {
 
-            const roleArr = [];
+    } else if (response.search === 'Update Employee') {
 
-            for (let i = 0; i < response.length; i++) {
-                roleArr.push(response[i].title);
-            }
+    } else if (response.search === 'View All Roles') {
 
-            // console.log(roleArr);
+    } else if (response.search === 'Add Role') {
 
-            inquirer.prompt([
-                {
-                    type: 'list',
-                    name: 'role',
-                    message: 'Which role would you like to search by?',
-                    choices: roleArr
-                }
-            ]).then(response => {
-                const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department
-                                FROM employee
-                                JOIN role ON employee.role_id = role.id
-                                JOIN department ON role.department_id = department.id
-                                WHERE role.title = ?`;
+    } else if (response.search === 'Update Role') {
 
-                let params = response.role;
-    
-                db.query(sql, params, (err, rows) => {
-                    if (err) {
-                        console.log(err);
-                    }
-    
-                    console.table(rows);
-                    menu();
-                    return;
-                });
-            });
-        });
-    }else if (response.search === 'Exit') {
+    } else if (response.search === 'View All Departments') {
+
+    } else if (response.search === 'Add Department') {
+
+    } else if (response.search === 'Update Department') {
+
+    } else if (response.search === 'Exit') {
         db.end();
         return;
     }
